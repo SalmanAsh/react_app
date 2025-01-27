0bab8e48754b9c5838481de02b81842e2af2690a
h45123sa <salman.ashraf@student.manchester.ac.uk>
2025-01-27 18:17:31 +0000

    Movie land

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import './App.css';
+import React from 'react';
+import { useEffect } from 'react';
+import "./App.css";
+import SearchIcon from "./search.svg";
 
+const API_URL = "https://www.omdbapi.com/?apikey=d67164fd"
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search["0"]);
+    
+  }
+  useEffect(() => {
+    searchMovies("Batman");
+  }, [])
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <h1>Movie States</h1>
+
+      <div className='search'>
+        <input 
+        placeholder='Search for a specific movie'
+        value="Batman"
+        onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="Search" onClick={() => {}}/>
+      </div>
+
+      <div className='container'>
+        <div className='movie'>
+          <div>
+            <p>Name</p>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
